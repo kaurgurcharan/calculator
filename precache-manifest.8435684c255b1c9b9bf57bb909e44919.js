e80f455049766d09990a91b488ea27b19220ebe4
Gurcharan Kaur <gurcharankaur@ravjots-air.lan>
2019-11-08 13:41:52 -0500

    Updates

---
+++ precache-manifest.8435684c255b1c9b9bf57bb909e44919.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d108d2b61aba3de59fe59201c89922",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "0294df3ffb6e87cf3903",
+    "url": "/calculator/static/css/main.e0fb489f.chunk.css"
+  },
+  {
+    "revision": "10db021f8a1400dcab1f",
+    "url": "/calculator/static/js/2.fd96d03f.chunk.js"
+  },
+  {
+    "revision": "0294df3ffb6e87cf3903",
+    "url": "/calculator/static/js/main.c24c1ac2.chunk.js"
+  },
+  {
+    "revision": "81eb61f5a8d26d2fd52d",
+    "url": "/calculator/static/js/runtime-main.1d26180e.js"
+  }
+]);
